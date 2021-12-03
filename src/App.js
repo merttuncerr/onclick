1834dee90b6cdd5a110cb7ec396ca2d5c7fa4411
Mert Tuncer <merttuncer1995@gmail.com>
2021-12-03 23:33:10 +0300

    onClick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      MyImage:'/images/immg1.jpg'
+    }
+  }
+  hello =(tabContentID) => {
+    if(tabContentID == 'button-1'){
+      this.setState({MyImage: '/images/immg1.jpg'});
+    }
+    if(tabContentID == 'button-2'){
+      this.setState({MyImage: '/images/mmg1.png'});
+    }
+
+  }
+  render() {
+    const{MyImage}=this.state;
+    return (
+      <div>
+        <div className="container">
+<div className="row">
+
+  <div className="col-lg-6">
+    <button onClick={() =>this.hello('button-1')}>resim1</button>
+    <button onClick={() =>this.hello('button-2')}>resim1</button>
+  </div>
+  <div className="col-lg-6"><img src={MyImage}/></div>
+</div>
+
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
